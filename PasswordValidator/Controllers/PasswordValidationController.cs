29cad828d2aee58b3542b25935cbfc5e5983859d
Harvey Marples <harveymarples@gmail.com>
2022-07-14 17:56:56 +0100

    Added PasswordValidationController and ValidatePassword controller method. Removed redundant WeatherForecast example Class and Controller.

---
+++ PasswordValidator/Controllers/PasswordValidationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordValidator.Controllers
+{
+    public class PasswordValidationController : Controller
+    {
+        [HttpGet]
+        [Route("ValidatePassword")]
+        public ActionResult ValidatePassword(string password)
+        {
+            bool isCorrectLength = (password.Length >= 6) && (password.Length <= 12);
+            bool containsNumber = password.Any(char.IsDigit);
+            bool containsSpecialCharacter = password.Any(character => !char.IsLetterOrDigit(character));
+
+            if (isCorrectLength && containsNumber && containsSpecialCharacter)
+            {
+                return Ok("Valid Password");
+            }
+            else
+            {
+                return BadRequest("Invalid Password");
+            } 
+        }
+    }
+}
